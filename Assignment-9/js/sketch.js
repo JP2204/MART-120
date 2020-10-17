866913441ff6230f33964ef395f96adc523edbb2
JP2204 <james.pierce@umontana.edu>
2020-10-17 15:04:43 -0400

    upload files

---
+++ Assignment-9/js/sketch.js
@@ -0,0 +1,44 @@
+function setup()
+{
+    createCanvas(500, 600);
+}
+
+function draw()
+{
+  background(159, 41, 144);
+  textSize(22)
+  text('What Is Self?', 10,100);
+
+// head
+  fill(55, 238, 194);
+  triangle(30, 220,208, 100, 360, 220);
+
+// eyes
+  strokeWeight(5);
+  fill(61, 233, 202);
+  point(150,95);
+  point(285,95);
+
+// body
+  fill(185, 129, 236);
+  rect(135,220,100,250);
+
+
+// right arm
+  line(130,260, 35, 305);
+// left arm
+  line(305,260, 35, 305);
+// left leg
+  fill(19, 207, 139)
+  rect(200,465,10,60);
+// right leg
+  fill(241, 122, 247)
+  rect(150,465,20,60);
+
+  fill(120);
+  textSize(22);
+  text("James Pierce",270,500 );
+
+
+
+}
